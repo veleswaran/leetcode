d006c398fe2a3c63aeb4102a79d58510a9a25154
veleswaran <vels344@gmail.com>
2023-04-05 18:13:08 +0530

    firste

---
+++ php1.php
@@ -0,0 +1,6 @@
+<?php
+$arr=array("name"=>"alexa","age"=>"6","robot"=>"Yes");
+print("my name is ".$arr['name']."\n");
+print("i am ".$arr['age']." old\n");
+print("are you a robot :".$arr['robot']);
+?>
